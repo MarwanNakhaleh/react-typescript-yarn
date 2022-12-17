ed6b4a8c17327b2635ae386d2b423956990f7469
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2022-12-17 18:37:00 -0500

    index.tsx and App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
