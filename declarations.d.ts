c8825df814848ec9a8d9eca3e7be0788c546761c
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2022-12-17 19:29:05 -0500

    it works

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,6 +3,6 @@ declare module "react/jsx-runtime";
 declare module "react-dom/client";
 declare namespace JSX {
     interface IntrinsicElements {
-        h1: {};
+        [elemName: string]: any;
     }
 }
